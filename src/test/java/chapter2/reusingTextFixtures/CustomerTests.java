816de2063727c179563e67b69d9cbce011a54c24
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-02-05 23:00:37 -0300

    feat: add reusing test fixtures example

---
+++ src/test/java/chapter2/reusingTextFixtures/CustomerTests.java
@@ -0,0 +1,50 @@
+package chapter2.reusingTextFixtures;
+
+import entities.Customer;
+import entities.Product;
+import entities.Store;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+
+// Extracting the common initialization code into private factory methods
+public class CustomerTests {
+    @Test
+    public void Purchase_succeeds_when_enough_inventory() {
+        // Arrange
+        Store store = createStoreWithInventory(Product.Shampoo, 10);
+        Customer customer = createCustomer();
+
+        // Act
+        boolean success = customer.purchase(store, Product.Shampoo, 5);
+
+        // Assert
+        assertTrue(success);
+        assertEquals(5, store.getInventory(Product.Shampoo));
+    }
+
+    @Test
+    public void Purchase_fails_when_not_enough_inventory() {
+        // Arrange
+        Store store = createStoreWithInventory(Product.Shampoo, 10);
+        Customer customer = createCustomer();
+
+        // Act
+        boolean success = customer.purchase(store, Product.Shampoo, 15);
+
+        // Assert
+        assertFalse(success);
+        assertEquals(10, store.getInventory(Product.Shampoo));
+    }
+
+    private Store createStoreWithInventory(Product product, int quantity) {
+        Store store = new Store();
+        store.addInventory(product, quantity);
+        return store;
+    }
+
+    private Customer createCustomer() {
+        return new Customer();
+    }
+}
