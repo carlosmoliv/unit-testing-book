09e08bfa1c0d4e91aca985cb22e3d155f66e4cb9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2024-03-07 10:10:48 -0300

    feat: add example of a test that is both a mock and a stub

---
+++ src/test/java/chapter5/bothAMockAndAStub/StoreTest.java
@@ -0,0 +1,30 @@
+package chapter5.bothAMockAndAStub;
+
+import entities.Customer2;
+import entities.IStore;
+import entities.Product;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.times;
+
+public class StoreTest {
+    // THe belo test is both a mock and a stub
+    // It returns a mocked response ad verifies a method call
+    // Sets up the answer from HasEnoughInventory() and verifies the call to RemoveInventory()
+    @Test
+    public void purchase_succeeds_when_enough_inventory() {
+        // Arrange
+        IStore storeMock = mock(IStore.class);
+        when(storeMock.hasEnoughInventory(Product.Shampoo, 5)).thenReturn(true);
+        Customer2 customer = new Customer2();
+
+        // Act
+        boolean success = customer.purchase(storeMock, Product.Shampoo, 5);
+
+        // Assert
+        assertTrue(success);
+        verify(storeMock, times(1)).removeInventory(Product.Shampoo, 5);
+    }
+}
